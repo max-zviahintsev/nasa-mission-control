bbe75e58780cba76f667ac21ebf331d5f94df29e
Max Zviagintsev <maxim.zviagintsev@gmail.com>
2024-12-25 21:25:58 +0200

    Setup eslint + prettier + ts

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -3,19 +3,33 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import eslintReact from 'eslint-plugin-react'
+import prettierPlugin from 'eslint-plugin-prettier'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...eslintPluginPrettierRecommended,
+    ],
+    ignores: ['dist', 'node_modules', 'eslint.config.js'],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ['tsconfig.json', 'tsconfig.node.json'],
+      },
     },
     plugins: {
+      react: eslintReact,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      '@typescript-eslint': tseslint.plugin,
+      prettier: prettierPlugin,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -24,5 +38,5 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
     },
-  },
+  }
 )
