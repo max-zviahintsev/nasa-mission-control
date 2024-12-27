56af7487382fa228f3e55651197537d2fe56a31b
Max Zviagintsev <maxim.zviagintsev@gmail.com>
2024-12-27 15:23:43 +0200

    Add text section to the Launch page

--- client/src/pages/launch/Launch.tsx
+++ client/src/pages/launch/Launch.tsx
@@ -1,3 +1,26 @@
+import { Wrapper, Paragraph, List } from './StyledComponents'
 export default function Launch() {
-  return <div>Launch</div>
+  return (
+    <div>
+      <Wrapper>
+        <Paragraph>
+          Schedule a mission launch for interstellar travel to one of the Kepler
+          Exoplanets.
+        </Paragraph>
+        <Paragraph>
+          Only confirmed planets matching the following criteria are available
+          for the earliest scheduled missions:
+        </Paragraph>
+        <Paragraph>
+          <List>
+            <li>Planetary radius less than 1.6 times Earth's radius</li>
+            <li>
+              Effective stellar flux more than 0.36 times Earth's value and less
+              than 1.11 times Earth's value
+            </li>
+          </List>
+        </Paragraph>
+      </Wrapper>
+    </div>
+  )
 }
---
+++ client/src/pages/launch/StyledComponents.ts
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled'
+
+export const Wrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  width: 800px;
+  margin: 20px auto;
+  padding: 20px;
+  border: 1px solid var(--main);
+  font-size: 21px;
+  color: var(--main);
+`
+export const Paragraph = styled.p`
+  margin: 0 0 20px 0;
+`
+export const List = styled.ul`
+  margin-bottom: 0;
+`
