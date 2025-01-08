3f13d93b2761011d480773c865e72b330f913cd1
Max Zviagintsev <maxim.zviagintsev@gmail.com>
2025-01-08 18:35:26 +0200

    Add tests for POST launches

--- server/src/routes/launches/launches.test.ts
+++ server/src/routes/launches/launches.test.ts
@@ -1,16 +1,51 @@
 import { describe, test, expect } from 'vitest'
-import request from 'supertest'
 import app from '../../app.ts'
 
 describe('Test Get /launches', () => {
-  test('Response should be 200', () => {
-    const response = request(app.server)
-    expect(response).to.equal(200)
+  test('Response should be 200', async () => {
+    const response = await app.inject({
+      method: 'GET',
+      url: '/launches',
+    })
+    expect(response.statusCode).to.equal(200)
+    expect(response.headers['content-type']).to.equal(
+      'application/json; charset=utf-8'
+    )
   })
 })
 describe('Test POST /launches', () => {
-  test('Response should be 200', () => {
-    const response = 88800
-    expect(response).to.equal(200)
+  const completeLaunchData = {
+    mission: 'USS Enterprise',
+    rocket: 'NCC 1701-D',
+    destination: 'Kepler-62 f',
+    launchDate: 'January 4, 2028',
+  }
+
+  const bodyWithoutDate = {
+    mission: 'USS Enterprise',
+    rocket: 'NCC 1701-D',
+    destination: 'Kepler-62 f',
+  }
+
+  test('Response should be 201 created', async () => {
+    const response = await app.inject({
+      method: 'POST',
+      url: '/launches',
+      body: completeLaunchData,
+    })
+
+    expect(response.statusCode).to.equal(201)
+    expect(response.headers['content-type']).to.equal(
+      'application/json; charset=utf-8'
+    )
+  })
+
+  test('Response should be 400', async () => {
+    const response = await app.inject({
+      method: 'POST',
+      url: '/launches',
+      body: bodyWithoutDate,
+    })
+    expect(response.statusCode).to.equal(400)
   })
 })
